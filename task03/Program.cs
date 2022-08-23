ea3de6e88bc00719e2a82bbdd625c51b702099ea
Ivan_Baryshev <obivanivan@mail.ru>
2022-08-23 17:51:50 +0300

    lesson06_update01

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,34 +1,38 @@
 ﻿// Задача 3. Со Звездочкой (*). Сгенерировать двумерный массив со случайными числами 
 
-
-Console.WriteLine("Чтобы сгенерировать двумерный массив со случайными числами ->");
-try
+void Random2DArrayCreation()
 {
-    Console.WriteLine("Введите высоту двумерного массива:");
-    int n = int.Parse(Console.ReadLine());
-    Console.WriteLine("Введите длину двумерного массива:");
-    int k = int.Parse(Console.ReadLine());
-    Console.WriteLine();
-    ArrayFilling(n, k);
-}
-catch
-{
-    Console.WriteLine("Вы ввели данные в неправильном формате!");
-}
-
-
-static void ArrayFilling(int n, int k)
-{
-    var array = new int[n, k];
-    var random = new Random();
-    for (int i = 0; i < n; i++)
+    Console.WriteLine("Чтобы сгенерировать двумерный массив со случайными числами ->");
+    try
     {
-        for (int j = 0; j < k; j++)
-        {
-            array[i, j] = random.Next(0, 10);
-            Console.Write(array[i, j] + " ");
-        }
+        Console.WriteLine("Введите высоту двумерного массива:");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите длину двумерного массива:");
+        int k = int.Parse(Console.ReadLine());
         Console.WriteLine();
+        ArrayFilling(n, k);
     }
-    Console.WriteLine("Массив сгенерирован");
+    catch
+    {
+        Console.WriteLine("Вы ввели данные в неправильном формате!");
+    }
+
+
+    static void ArrayFilling(int n, int k)
+    {
+        var array = new int[n, k];
+        var random = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                array[i, j] = random.Next(0, 10);
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Массив сгенерирован");
+    }
 }
+
+Random2DArrayCreation();
